7d74a6c308f2d05406df73b01d49e1198384bdb9
Vivek Chaudhary <vivek.experiotech@gmail.com>
2022-10-03 02:46:25 +0000

    black code formated

--- infer.py
+++ infer.py
@@ -19,29 +19,30 @@ IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
-        
+
         self.labels2class = json.load(open("imagenet1000_labels.json", "r"))
-        self.augs = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(), 
-            transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD)
-        ])
-        self.model = create_model(
-            "efficientnet_b3a", pretrained=True)
+        self.augs = transforms.Compose(
+            [
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD),
+            ]
+        )
+        self.model = create_model("efficientnet_b3a", pretrained=True)
 
     # Define the arguments and types the model takes as input
-    def predict(self,image: Path = Input(description="Image to classify")) -> Any:
+    def predict(self, image: Path = Input(description="Image to classify")) -> Any:
         """Run a single prediction on the model"""
 
-       
         img = Image.open(image).convert("RGB")
-        
+
         # Preprocess the image
         img = self.augs(img).unsqueeze(0)
         out = self.model(img)
         out = softmax(out, dim=1)
         idx = out.argmax().item()
 
-        return {"predicted": self.labels2class[str(idx)], "confidence": out[0,idx].item()}
-
-
+        return {
+            "predicted": self.labels2class[str(idx)],
+            "confidence": out[0, idx].item(),
+        }
