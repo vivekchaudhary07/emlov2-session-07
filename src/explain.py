0b4ad75ed5c57133946863f7518c43eb01e38b61
Vivek Chaudhary <vivek.experiotech@gmail.com>
2022-11-12 17:34:48 +0530

    multiple files

--- src/explain.py
+++ src/explain.py
@@ -8,6 +8,7 @@ root = pyrootutils.setup_root(
 )
 
 import os
+from glob import glob
 
 import hydra
 import numpy as np
@@ -28,34 +29,39 @@ np.random.seed(0)
 )
 def main(cfg: DictConfig):
     print(cfg)
-    out_path = os.path.join("images/modelexplainablity_outs", os.path.basename(cfg.image_path))
-    os.makedirs(out_path, exist_ok=True)
 
-    device = torch.device("cuda")
+    sources = [cfg.source] if os.path.isfile(cfg.source) else glob(f"{cfg.source}/*")
+    print(len(sources))
+    for image_path in sources:
+        print(image_path)
+        out_path = os.path.join("images/modelexplainablity_outs", os.path.basename(image_path))
+        os.makedirs(out_path, exist_ok=True)
 
-    transform = T.Compose([T.Resize(cfg.imput_im_size), T.ToTensor()])
-    transform_normalize = T.Normalize(mean=cfg.MEAN, std=cfg.STD)
+        device = torch.device("cuda")
 
-    model: LightningModule = hydra.utils.instantiate(cfg.model)
-    model = model.to(device)
-    model_explain = hydra.utils.instantiate(cfg.explainability)
+        transform = T.Compose([T.Resize(cfg.imput_im_size), T.ToTensor()])
+        transform_normalize = T.Normalize(mean=cfg.MEAN, std=cfg.STD)
 
-    img = Image.open(cfg.image_path)
-    transformed_img = transform(img)
-    img_tensor = transform_normalize(transformed_img).unsqueeze(0).to(device)
+        model: LightningModule = hydra.utils.instantiate(cfg.model)
+        model = model.to(device)
+        model_explain = hydra.utils.instantiate(cfg.explainability)
 
-    output = model(img_tensor)
-    output = F.softmax(output, dim=1)
-    prediction_score, pred_label_idx = torch.topk(output, 1)
-    # print(type(pred_label_idx))
+        img = Image.open(image_path)
+        transformed_img = transform(img)
+        img_tensor = transform_normalize(transformed_img).unsqueeze(0).to(device)
 
-    model_explain(
-        model=model,
-        img_tensor=img_tensor,
-        np_img=np.transpose(transformed_img.cpu().detach().numpy(), (1, 2, 0)),
-        pred_label_idx=pred_label_idx,
-        save_path=out_path,
-    )
+        output = model(img_tensor)
+        output = F.softmax(output, dim=1)
+        prediction_score, pred_label_idx = torch.topk(output, 1)
+        # print(type(pred_label_idx))
+
+        model_explain(
+            model=model,
+            img_tensor=img_tensor,
+            np_img=np.transpose(transformed_img.cpu().detach().numpy(), (1, 2, 0)),
+            pred_label_idx=pred_label_idx,
+            save_path=out_path,
+        )
 
 
 if __name__ == "__main__":
